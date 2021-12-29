6210002d7cf5c7369329eaa2d4f6c520af8a5a81
Nguyen Sy Bao <nguyensybao1403@gmail.com>
2021-12-29 15:44:34 +0700

    update comment rate

--- src/main/java/com/pihotel/repository/ICommentRepo.java
+++ src/main/java/com/pihotel/repository/ICommentRepo.java
@@ -9,3 +9,10 @@ public interface ICommentRepo extends JpaRepository<CommentEntity, String>{
 	public CommentEntity findOneById(String id);
 	
 }
+
+/*
+ * select distinct c.* from invoice i inner join invoice_room ir on i.id =
+ * ir.id_invoice inner join account a on a.id = i.id_account inner join comment
+ * c on a.id = c.id_account inner join room r on r.id = c.id_room where i.id =
+ * 'escwYA58f07f' and i.[enabled] = 1 and c.commented = 0
+ */
